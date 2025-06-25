c033da1aad072ccc7a440017a4e81b52ba3b45f0
vaibhavr21 <vaibhavrmaurya2004@gmail.com>
2025-06-25 20:02:27 +0530

    allowedOrigins

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ const _dirname = path.resolve();
 
 
 // allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://greencart-yaok.onrender.com'
+];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebHooks)
 
